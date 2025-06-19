d78536919fdc27832c3bd8984397f7f6fa98365a
jgarner9 <garnerjasonj@icloud.com>
2025-06-19 16:28:28 -0600

    add keymaps to nvim config

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -1,9 +1,9 @@
--- Bootstrap lazy.nvim
+--  wjootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
+	if wjim.v.shell_error ~= 0 then
 		vim.api.nvim_echo({
 			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
 			{ out,                            "WarningMsg" },
@@ -15,9 +15,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.g.loaded_netrw = 1
@@ -28,15 +25,24 @@ vim.opt.number = true
 
 vim.loader.enable()
 
+-- include custom keymaps
+-- vim.keymap.set("n", "<leader>ttt", "<cmd>echo 'test'<cr>")
+vim.keymap.set("n", "<leader>wh", "<C-w>h")
+vim.keymap.set("n", "<leader>wj", "<C-w>j")
+vim.keymap.set("n", "<leader>wk", "<C-w>k")
+vim.keymap.set("n", "<leader>wl", "<C-w>l")
+vim.keymap.set("n", "<leader>wv", "<cmd>vsplit<cr>")
+vim.keymap.set("n", "<leader>ws", "<cmd>split<cr>")
+vim.keymap.set("n", "<leader>wq", "<cmd>close<cr>")
+vim.keymap.set("n", "<leader>ww", "<cmd>wq<cr>")
+vim.keymap.set("n", "<cr>", "o<esc>")
+vim.keymap.set("n", "<leader>\\", "<cmd>split<cr><C-w>j<cmd>terminal<cr>a")
+
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
 		-- import your plugins
 		{ import = "plugins" },
 	},
-	-- Configure any other settings here. See the documentation for more details.
-	-- colorscheme that will be used when installing plugins.
-	-- colorscheme = { "kanagawa-wave" },
-	-- automatically check for plugin updates
 	checker = { enabled = true },
 })
