2878a43ffbfb0c13cfa7b82fd8832d1b722e2ccc
Jason <garnerjasonj@icloud.com>
2025-06-27 17:04:49 +0000

    add lspsaga plugin and update keymaps

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -35,6 +35,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.lazyredraw = true
 vim.opt.number = false
 vim.opt.relativenumber = false
+vim.opt.signcolumn = "no"
 vim.loader.enable()
 
 -- Enable line numbers only for code-like filetypes
@@ -60,19 +61,28 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 
 -- include custom keymaps
 -- vim.keymap.set("n", "<leader>ttt", "<cmd>echo 'test'<cr>")
+-- Window navigation
 vim.keymap.set("n", "<leader>wh", "<C-w>h")
 vim.keymap.set("n", "<leader>wj", "<C-w>j")
 vim.keymap.set("n", "<leader>wk", "<C-w>k")
 vim.keymap.set("n", "<leader>wl", "<C-w>l")
+-- Window management
 vim.keymap.set("n", "<leader>wv", "<cmd>vsplit<cr>")
 vim.keymap.set("n", "<leader>ws", "<cmd>split<cr>")
 vim.keymap.set("n", "<leader>wq", "<cmd>close<cr>")
+vim.keymap.set("n", "<leader>wc", "<cmd>bd<cr>")
+-- saving and quitting
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
 vim.keymap.set("n", "<leader>ww", "<cmd>wq<cr>")
 vim.keymap.set("n", "<leader>qq", "<cmd>q!<cr>")
+-- QoL keymaps
 vim.keymap.set("n", "<cr>", "o<esc>")
+-- la Terminal
 vim.keymap.set("n", "<leader>\\", "<cmd>split<cr><C-w>j<cmd>terminal<cr>a")
-vim.keymap.set("n", "<leader>wc", "<cmd>bd<cr>")
+-- Lspsaga keymaps
+vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga peek_definition<cr>")
+vim.keymap.set("n", "<leader>g", "<cmd>Lspsaga goto_definition<cr>")
+vim.keymap.set("n", "<leader>lf", "<cmd>Lspsaga finder<cr>")
 
 -- Setup lazy.nvim
 require("lazy").setup({
